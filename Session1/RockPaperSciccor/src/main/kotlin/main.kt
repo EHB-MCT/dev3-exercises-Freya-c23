abb4590ab2f5e47bb46f99757c75a7437de380e8
Freya-c23 <freya.claes@student.ehb.be>
2021-10-07 16:48:08 +0200

    Works, but not the stop and there is 2 times input

--- Session1/RockPaperSciccor/src/main/kotlin/main.kt
+++ Session1/RockPaperSciccor/src/main/kotlin/main.kt
@@ -1,2 +1,28 @@
-class main {
+fun main(args: Array<String>){
+    print("choose: rock, paper or scissor: ")
+    val enteredString = readLine()
+    while (enteredString != "stop"){
+        print("choose: rock, paper or scissor: ")
+        val enteredString = readLine()
+
+        val options = arrayOf("rock","paper","scissor")
+        var comp = options.random()
+        println("computer says: $comp")
+
+        if (comp == enteredString){
+            println("Tie")
+        } else if(comp == "rock" && enteredString == "scissor"){
+            println("You lose")
+        }else if(comp == "rock" && enteredString == "paper"){
+            println("You win")
+        }else if(comp == "paper" && enteredString == "scissor"){
+            println("You win")
+        }else if(comp == "paper" && enteredString == "rock"){
+            println("You lose")
+        }else if(comp == "scissor" && enteredString == "paper"){
+            println("You lose")
+        }else if(comp == "scissor" && enteredString == "rock"){
+            println("You win")
+        }
+    }
 }
