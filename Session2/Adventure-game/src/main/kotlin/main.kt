ecc750c4b2e342a7df4822d30b756b0bb9b85ca1
Freya-c23 <freya.claes@student.ehb.be>
2021-10-14 15:26:13 +0200

    Level 1 of the game
    the game is based on spyro and the first level is a question with multiple answers

--- Session2/Adventure-game/src/main/kotlin/main.kt
+++ Session2/Adventure-game/src/main/kotlin/main.kt
@@ -1,2 +1,23 @@
-class main {
+fun main(){
+    println("Hello world")
+    challengeOne()
+}
+fun challengeOne(){
+    println("Intro challenge 1")
+    println("Welcome player! \n If you are a Spyro fan, this game is perfect for you! \n for the first question: name one of the 5 main characters of Spyro: year of the dragon (except Spyro ofc)")
+
+    val answers = setOf<String>("Sparx", "Hunter", "Sheila", "Sergeant Byrd", "Bentley")
+    val userAnswer = readLine()
+    if (answers.contains(userAnswer)){
+        win()
+    }else{
+        gameOver()
+    }
+}
+
+fun win(){
+    println("WOHOOO you can go to the next level now!")
+}
+fun gameOver(){
+    println("BOOOO GAME OVER!!")
 }
