eaac367a871e1b2f600092b50636074911b8a634
Freya-c23 <freya.claes@student.ehb.be>
2021-10-14 16:46:33 +0200

    challenge three and four
    forgot to commit challenge three, but did all this in not much time. Also optimized gameplay

--- Session2/Adventure-game/src/main/kotlin/main.kt
+++ Session2/Adventure-game/src/main/kotlin/main.kt
@@ -3,9 +3,6 @@ import kotlin.random.Random
 fun main(){
     println("Hello world")
     challengeOne()
-    if(challengeOne() == win()){
-        challengeTwo()
-    }
 }
 fun challengeOne(){
     println("Intro challenge 1")
@@ -14,7 +11,7 @@ fun challengeOne(){
     val answers = setOf<String>("Sparx", "Hunter", "Sheila", "Sergeant Byrd", "Bentley")
     val userAnswer = readLine()
     if (answers.contains(userAnswer)){
-        win()
+        challengeTwo()
     }else{
         gameOver()
     }
@@ -26,7 +23,7 @@ fun challengeTwo(){
     val userAnswer = readLine();
 
     if (rollDice() == userAnswer){
-        win()
+        challengeThree()
     }else{
         gameOver()
     }
@@ -40,8 +37,51 @@ fun rollDice():String{
     }
 }
 
+fun challengeTwoRepeat(){
+    println("You return to a place you already know \n The Gnorc has made his way back to block the area and has forgotten how you look so you pass again " +
+            "\n he will only let you pass if you choose the right number! \n " +
+            "You can choose to answer: \"low\" (between 1-3) or \"high\" (between 4-6)")
+    val userAnswer = readLine();
 
+    if (rollDice() == userAnswer){
+        challengeThree()
+    }else{
+        gameOver()
+    }
+}
+fun challengeThree(){
+    println("YES you made it past the Gnorc! \n Ohno! the Gnorc was so mad that you beat him at his game that he is coming after you" +
+            "\n You can choose an attackmode to get him before he gets you \n " +
+            "You can choose between: \"super charge\", \"flying\",  or \"super fire\" ")
 
+    val userAnswer = readLine();
+    if (userAnswer == "flying"){
+        println("You fled from the Gnorc, you must go back a level")
+        challengeTwoRepeat()
+    }else if(userAnswer == "super charge"){
+        println("You charged the Gnorc to the other side of the realm, you can proceed!")
+        challengeFour()
+    }else if(userAnswer == "super fire"){
+        println("The Gnorc was immune for your fire.")
+        gameOver()
+    }
+}
+
+fun challengeFour(){
+    println("You are almost at the end of your adventure \n " +
+            "Before you can get to the final boss, you must choose the right portal to go trough \n" +
+            "there are a hundred portals, so you must guess the right one (type in digits)")
+    val userAnswer = readLine()
+    val random = Random.nextInt(1,100)
+    val randomString = random.toString()
+
+    if (userAnswer == randomString){
+        println("You found the right portal!")
+        challengeFive()
+    }
+}
+
+che
 fun win(){
     println("WOHOOO you can go to the next level now!")
 }
